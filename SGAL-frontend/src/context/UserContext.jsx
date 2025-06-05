0b8976a39968f8f6f7024edbcc7150811792c925
FevoRenndell <cristianreyessaez@outlook.com>
2025-06-05 17:49:25 -0400

    Corrección de Usercontext.jsx para tener acceso global a User, Token, login() y logout().

--- SGAL-frontend/src/context/UserContext.jsx
+++ SGAL-frontend/src/context/UserContext.jsx
@@ -1,32 +1,38 @@
-
 import React, { createContext, useState, useEffect } from 'react';
 
 export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const [token, setToken] = useState(null);
 
+  // Cargar datos desde localStorage al inicio
   useEffect(() => {
-    // Recuperar datos del usuario al cargar la aplicación
     const storedUser = localStorage.getItem('user');
-    if (storedUser) {
-      setUser(JSON.parse(storedUser));
-    }
+    const storedToken = localStorage.getItem('token');
+
+    if (storedUser) setUser(JSON.parse(storedUser));
+    if (storedToken) setToken(storedToken);
   }, []);
 
-  const updateUser = (userData) => {
+  // Función para iniciar sesión y guardar token + usuario
+  const login = (token, userData) => {
+    localStorage.setItem('token', token);
     localStorage.setItem('user', JSON.stringify(userData));
+    setToken(token);
     setUser(userData);
   };
 
   const logout = () => {
+    localStorage.removeItem('token');
     localStorage.removeItem('user');
+    setToken(null);
     setUser(null);
   };
 
   return (
-    <UserContext.Provider value={{ user, updateUser, logout }}>
-      {children}
-    </UserContext.Provider>
+      <UserContext.Provider value={{ user, token, login, logout }}>
+        {children}
+      </UserContext.Provider>
   );
 };
