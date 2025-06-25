fd31560524e00e6752c471ea3d5bdb8f384a3b57
gaspiii <osoriogasparr@gmail.com>
2025-06-24 23:47:45 -0400

    orden

---
+++ SGAL-frontend/src/api/axios.js
@@ -0,0 +1,9 @@
+// src/api/axios.js
+import axios from "axios";
+
+const API = axios.create({
+  baseURL: "http://localhost:3000/api", // Ya incluye /api
+  withCredentials: true,
+});
+
+export default API;
